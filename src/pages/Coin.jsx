3206b837555b122dc2c6dbf768ede44850aed40f
Natnael <natnaelgetachew814@gmail.com>
2025-05-26 12:18:03 +0300

    another commit

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -3,12 +3,13 @@ import "../pages/coin.css";
 import { useParams } from "react-router-dom";
 import { CoinContext } from "../context/CoinContext";
 import LionChart from "../components/LionChart";
+
 function Coin() {
   const { coinId } = useParams();
   const [coinData, seCoinData] = useState();
   const [historicalcoinData, seHistoricalCoinData] = useState();
-
   const { currency } = useContext(CoinContext);
+
   const fetchCoinData = async () => {
     try {
       const res = await fetch(
@@ -20,6 +21,7 @@ function Coin() {
       console.error("Failed to fetch coin data:", err);
     }
   };
+
   const fetchHistoricalData = async () => {
     try {
       const res = await fetch(
@@ -31,71 +33,64 @@ function Coin() {
       console.error("Failed to fetch historical data:", err);
     }
   };
-  useEffect(
-    function () {
-      fetchCoinData();
-      fetchHistoricalData();
-    },
-    [currency]
+
+  useEffect(() => {
+    fetchCoinData();
+    fetchHistoricalData();
+  }, [currency]);
+
+  // Only render if both data are loaded
+  if (!coinData || !historicalcoinData) return null;
+
+  return (
+    <div className="coin">
+      <div className="coin-name">
+        <img src={coinData.image.large} alt={coinData.name} />
+        <p>
+          <b>
+            {coinData.name} {coinData.symbol.toUpperCase()}
+          </b>
+        </p>
+      </div>
+      <div className="coin-chart">
+        <LionChart historicalcoinData={historicalcoinData} />
+      </div>
+      <div className="coin-info">
+        <ul>
+          <li>Crypto Market Rank</li>
+          <li>{coinData.market_cap_rank}</li>
+        </ul>
+        <ul>
+          <li>Current Price</li>
+          <li>
+            {currency.symbol}
+            {coinData.market_data.current_price[currency.name].toLocaleString()}
+          </li>
+        </ul>
+        <ul>
+          <li>Market Cap</li>
+          <li>
+            {currency.symbol}
+            {coinData.market_data.market_cap[currency.name].toLocaleString()}
+          </li>
+        </ul>
+        <ul>
+          <li>24 Hour High</li>
+          <li>
+            {currency.symbol}
+            {coinData.market_data.high_24h[currency.name].toLocaleString()}
+          </li>
+        </ul>
+        <ul>
+          <li>24 Hour Low</li>
+          <li>
+            {currency.symbol}
+            {coinData.market_data.low_24h[currency.name].toLocaleString()}
+          </li>
+        </ul>
+      </div>
+    </div>
   );
-  if ((coinData, historicalcoinData)) {
-    return (
-      <div className="coin">
-        <div className="coin-name">
-          <img src={coinData.image.large} alt="" />
-          <p>
-            <b>
-              {coinData.name} {""} {coinData.symbol.toUpperCase()}
-            </b>
-          </p>
-        </div>
-        <div className="coin-chart">
-          <LionChart historicalcoinData={historicalcoinData} />
-        </div>
-        <div className="coin-info">
-          <ul>
-            <li>Crypto Market Rank</li>
-            <li>{coinData.market_cap_rank}</li>
-          </ul>
-          <ul>
-            <li>Current Price</li>
-            <li>
-              {currency.symbol}
-              {coinData.market_data.current_price[
-                currency.name
-              ].toLocaleString()}
-            </li>
-          </ul>
-          <ul>
-            <li>Market cap</li>
-            <li>
-              {currency.symbol}
-              {coinData.market_data.market_cap[currency.name].toLocaleString()}
-            </li>
-          </ul>
-          <ul>
-            <li>24 Hour high</li>
-            <li>
-              {currency.symbol}
-              {coinData.market_data.high_24h[currency.name].toLocaleString()}
-            </li>
-          </ul>
-          <ul>
-            <li>24 Hour low</li>
-            <li>
-              {currency.symbol}
-              {coinData.market_data.low_24h[currency.name].toLocaleString()}
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="spinner">
-        <div className="spin"></div>
-      </div>
-    );
-  }
 }
+
 export default Coin;
